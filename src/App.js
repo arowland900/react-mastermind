908e38ba60e8af158b0ba99ff31e887ccb92bd57
Alexander Rowland <arowland900@gmail.com>
2019-06-26 10:44:09 -0700

    add game timer and new game button components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import GameBoard from './components/GameBoard/GameBoard'
 import ColorPicker from './components/ColorPicker/ColorPicker'
+import GameTimer from './components/GameTimer/GameTimer'
+import NewGameButton from './components/NewGameButton/NewGameButton'
 
-class App extends React.Component {
+class App extends Component {
   render() {
-
-
     return (
       <div className="App">
         <header className="App-header">
@@ -14,6 +14,8 @@ class App extends React.Component {
         </header>
         <GameBoard />
         <ColorPicker />
+        <GameTimer />
+        <NewGameButton />
       </div>
     );
   }
