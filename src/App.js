6af68156e09515b0daf1ea97846f9788fd209cd5
Alexander Rowland <arowland900@gmail.com>
2019-06-26 13:40:43 -0700

    add button for set state on app js

--- src/App.js
+++ src/App.js
@@ -6,34 +6,47 @@ import GameTimer from './components/GameTimer/GameTimer'
 import NewGameButton from './components/NewGameButton/NewGameButton'
 import Footer from './components/Footer/Footer'
 
-const colors = ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD']
+const colors = ['blue', 'green', 'red', 'yellow']
+
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      selColorIdx: 0
-    }
-  }
-  render() {
-    return (
-      <div className="App">
-        Selected Color: {colors[this.state.selColorIdx]}
-        <header className="App-header">
-          REACT MASTERMIND~!
-        </header>
-        <div className="flex-container">
-          <GameBoard />
-          <div>
-            <ColorPicker />
-            <GameTimer />
-            <NewGameButton />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+	constructor() {
+		super();
+		this.state = {
+			selColorIdx: 0,
+			guesses: [],
+			code: this.generateCode()
+		}
+	};
+	generateCode() {
+		return new Array(4).fill().map(() => Math.floor(Math.random() * 4));
+	};
+	render() {
+		return (
+			<div className="App">
+				<button onClick={() => this.setState((state) => {
+					return {
+						selColorIdx: ++state.selColorIdx % 4
+					};
+				})}>
+					Next Color
+				</button>
+				Selected Color: {colors[this.state.selColorIdx]}
+				<header className="App-header">
+					REACT MASTERMIND~!
+       			 </header>
+				<div className="flex-container">
+					<GameBoard />
+					<div>
+						<ColorPicker colors={colors} />
+						<GameTimer />
+						<NewGameButton />
+					</div>
+				</div>
+				<Footer />
+			</div>
+		);
+	}
 }
 
 export default App;
