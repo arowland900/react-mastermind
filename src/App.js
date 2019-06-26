6b845d824488a8a184ae34ab05b003c8717cbf42
Alexander Rowland <arowland900@gmail.com>
2019-06-26 13:07:11 -0700

    add colors array & state

--- src/App.js
+++ src/App.js
@@ -6,10 +6,19 @@ import GameTimer from './components/GameTimer/GameTimer'
 import NewGameButton from './components/NewGameButton/NewGameButton'
 import Footer from './components/Footer/Footer'
 
+const colors = ['#7CCCE5', '#FDE47F', '#E04644', '#B576AD']
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      selColorIdx: 0
+    }
+  }
   render() {
     return (
       <div className="App">
+        Selected Color: {colors[this.state.selColorIdx]}
         <header className="App-header">
           REACT MASTERMIND~!
         </header>
